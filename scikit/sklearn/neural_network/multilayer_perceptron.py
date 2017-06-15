e7215a8ea1ec1e296616670b882c8bb8917e45b8
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2017-06-15 19:50:26 +0800

    back to full test mode

--- scikit/sklearn/neural_network/multilayer_perceptron.py
+++ scikit/sklearn/neural_network/multilayer_perceptron.py
@@ -249,33 +249,26 @@ class BaseMultilayerPerceptron(six.with_metaclass(ABCMeta, BaseEstimator)):
                   intercept_grads):
         """Compute the MLP loss function and its corresponding derivatives
         with respect to each parameter: weights and bias vectors.
-
         Parameters
         ----------
         X : {array-like, sparse matrix}, shape (n_samples, n_features)
             The input data.
-
         y : array-like, shape (n_samples,)
             The target values.
-
         activations : list, length = n_layers - 1
              The ith element of the list holds the values of the ith layer.
-
         deltas : list, length = n_layers - 1
             The ith element of the list holds the difference between the
             activations of the i + 1 layer and the backpropagated error.
             More specifically, deltas are gradients of loss with respect to z
             in each layer, where z = wx + b is the value of a particular layer
             before passing through the activation function
-
         coef_grad : list, length = n_layers - 1
             The ith element contains the amount of change used to update the
             coefficient parameters of the ith layer in an iteration.
-
         intercept_grads : list, length = n_layers - 1
             The ith element contains the amount of change used to update the
             intercept parameters of the ith layer in an iteration.
-
         Returns
         -------
         loss : float
@@ -285,23 +278,17 @@ class BaseMultilayerPerceptron(six.with_metaclass(ABCMeta, BaseEstimator)):
         n_samples = X.shape[0]
 
         # Forward propagate
-        tss = time.clock()
         activations = self._forward_pass(activations)
 
-        ts = time.clock()
         # Get loss
         loss_func_name = self.loss
         if loss_func_name == 'log_loss' and self.out_activation_ == 'logistic':
             loss_func_name = 'binary_log_loss'
         loss = LOSS_FUNCTIONS[loss_func_name](y, activations[-1])
         # Add L2 regularization term to loss
-        ts = time.clock()
         values = np.sum(
             np.array([np.dot(s.ravel(), s.ravel()) for s in self.coefs_]))
         loss += (0.5 * self.alpha) * values / n_samples
-
-        if self.fitting:
-            print("CPU process regularization function: %f ms \n" % ((time.clock() - ts) * 1000)) 
 
         # Backward propagate
         last = self.n_layers_ - 2
@@ -310,20 +297,11 @@ class BaseMultilayerPerceptron(six.with_metaclass(ABCMeta, BaseEstimator)):
         # combinations of output activation and loss function:
         # sigmoid and binary cross entropy, softmax and categorical cross
         # entropy, and identity with squared loss
-        
-        ts = time.clock()
         deltas[last] = activations[-1] - y
-
-        if self.fitting:
-            print("CPU process delta function: %f ms \n" % ((time.clock() - ts) * 1000)) 
-
-        
-        
 
         # Compute gradient for the last layer
         coef_grads, intercept_grads = self._compute_loss_grad(
             last, n_samples, activations, deltas, coef_grads, intercept_grads)
-
 
         # Iterate over the hidden layers
         for i in range(self.n_layers_ - 2, 0, -1):
@@ -335,9 +313,8 @@ class BaseMultilayerPerceptron(six.with_metaclass(ABCMeta, BaseEstimator)):
                 i - 1, n_samples, activations, deltas, coef_grads,
                 intercept_grads)
 
-        if self.fitting:
-            print("CPU Total Backprop: %f ms \n" % ((time.clock() - tss) * 1000))
         return loss, coef_grads, intercept_grads
+
 
     def _backprop_cuda(self, X, y, activations, deltas, coef_grads,
                   intercept_grads):
@@ -863,8 +840,8 @@ class BaseMultilayerPerceptron(six.with_metaclass(ABCMeta, BaseEstimator)):
                             self.cuda_coefs_[i].set(self.coefs_[i])
                         for i in range(0, len(self.intercepts_)):
                             self.cuda_intercepts_[i].set(self.intercepts_[i])
-                    if batch_slice.start > 1000:
-                        break # FIXME:force one batch
+                    #if batch_slice.start > 1000:
+                    #    break # FIXME:force one batch
                     
                     
                 
